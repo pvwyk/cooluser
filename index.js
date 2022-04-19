cde01e2fbeb066621e621a3beca27397b06efaf6
pvwyk <pvanwyk07@gmail.com>
2022-04-19 11:14:43 +0200

    just an update

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 let animalNames = [ "kudu", "Ant", "Antelope", "Baboon", "Bat", "Beagle", "Bear", "Bird", "Butterfly", "Cat", "Caterpillar", "Chicken","Cow", "Dog", "Dolphin", "Donkey", "Eagle", "Fish", , "Fox", "Frog", "Gerbil", "Goose", "Gopher", "Gorilla", "Heron", "HoneyBee",]
-let descriptiveNames = ["Arobatic","Adorable","Adventurous","Brave","Bright","Brilliant","Concrete","Conventional","Foolhardy","Gregarious","Grim","Handsome","Handy","Intelligent","Intrepid","Joyful","Jubilant","Keen","Kooky","Lanky","Lazy","Limp","Luxurious","Mediocre","Mellow","Miserable","Nocturnal","Organic","Ornate","Ordinary","Powerless","Practical","Precious","Questionable" ,"Quirky" ,"Radiant","Rustic","Sly","Sophisticated","Stunning","Tattered","Horny","Weathered"]
+let descriptiveNames = ["Acrobatic","Adorable","Adventurous","Brave","Bright","Brilliant","Concrete","Conventional","Foolhardy","Gregarious","Grim","Handsome","Handy","Intelligent","Intrepid","Joyful","Jubilant","Keen","Kooky","Lanky","Lazy","Limp","Luxurious","Mediocre","Mellow","Miserable","Nocturnal","Organic","Ornate","Ordinary","Powerless","Practical","Precious","Questionable" ,"Quirky" ,"Radiant","Rustic","Sly","Sophisticated","Stunning","Tattered","Horny","Weathered"]
 let resultEl = document.getElementById("result-el")
 
-function coolName(){
+
+function coolName(){ //function to get a random name from the two above arrays and put them together in a string
         let getOne = Math.floor(Math.random() * animalNames.length)
         let getTwo = Math.floor(Math.random() * descriptiveNames.length)
+        console.log(getOne)
+        console.log(getTwo)
 
         let randomAnimal = animalNames[getOne]
         let randomDescription = descriptiveNames[getTwo]
+        console.log(randomAnimal)
+        console.log(randomAnimal)
 
         let userName = randomDescription + randomAnimal
         console.log(userName)
         resultEl.textContent = userName
-        
+        }
 
-}
-
-console.log("test this")
